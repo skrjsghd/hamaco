069a84eedbe45ce1cb296064c717011e744aedfe
Ghong <alsrjsghd1@gmail.com>
2025-09-25 15:18:25 +0900

    add image previews in result page

--- src/app/result/[id]/page.tsx
+++ src/app/result/[id]/page.tsx
@@ -22,17 +22,18 @@ export default async function ResultPage({
   }
 
   return (
-    <div className="max-w-lg mx-auto px-4 min-h-svh flex flex-col justify-center items-center">
-      <Image
-        src={
-          process.env.NEXT_PUBLIC_SUPABASE_URL + "/storage/v1/object/public/" + result.hairstyles[0].imageUrl
-        }
-        alt={result.hairstyles[0].name}
-        sizes="100vw"
-        width={500}
-        height={300}
-        className="w-full"
-      />
+    <div className="max-w-lg mx-auto px-4 min-h-svh grid grid-cols-2 gap-4">
+      {result.hairstyles.map((hairstyle) => (
+        <Image
+          key={hairstyle.imageUrl}
+          src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${hairstyle.imageUrl}`}
+          alt={hairstyle.name}
+          sizes="100vw"
+          width={500}
+          height={300}
+          className="w-full"
+        />
+      ))}
     </div>
   );
 }
